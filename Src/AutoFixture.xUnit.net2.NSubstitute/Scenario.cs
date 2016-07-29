ea1c0596b6fcd2ee8d5eb1d0166a31b381be4afc
sergeyshushlyapin <sshushlyapin@gmail.com>
2016-07-30 01:35:05 +0300

    Create integraton test project to combine xUnit.net2 and NSubstitute tests

---
+++ Src/AutoFixture.xUnit.net2.NSubstitute/Scenario.cs
@@ -0,0 +1,42 @@
+﻿using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoNSubstitute;
+using Ploeh.AutoFixture.AutoNSubstitute.UnitTest.TestTypes;
+using Ploeh.AutoFixture.Xunit2;
+using Ploeh.TestTypeFoundation;
+using Xunit;
+
+namespace AutoFixture.xUnit.net2.NSubstitute
+{
+    public class Scenario
+    {
+        [Theory, AutoNSubstituteData]
+        public void CreateWithConcreteTypeAndSubstituteAttributeCreatesMock([Substitute] ConcreteType sut)
+        {
+            Assert.IsAssignableFrom<ConcreteType>(sut);
+            Assert.True(sut.GetType().BaseType == typeof(ConcreteType));
+        }
+
+        [Theory, AutoNSubstituteData]
+        public void CreateWithFrozenConcreteTypeAndSubstituteAttributeCreatesMock([Frozen, Substitute] ConcreteType sut)
+        {
+            Assert.IsAssignableFrom<ConcreteType>(sut);
+            Assert.True(sut.GetType().BaseType == typeof(ConcreteType));
+        }
+
+        [Theory, AutoNSubstituteData]
+        public void CreateWithInterfaceAndFrozenByPropertyNameAttributeFreezesProperty(
+            [Frozen(Matching.PropertyName)] string property,
+            IInterfaceWithProperty sut)
+        {
+            Assert.Same(property, sut.Property);
+        }
+
+        private class AutoNSubstituteDataAttribute : AutoDataAttribute
+        {
+            public AutoNSubstituteDataAttribute()
+              : base(new Fixture().Customize(new AutoConfiguredNSubstituteCustomization()))
+            {
+            }
+        }
+    }
+}
