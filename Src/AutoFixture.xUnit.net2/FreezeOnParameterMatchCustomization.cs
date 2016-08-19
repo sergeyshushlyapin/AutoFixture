5cad857f21386bc8ec024b90a9e1b775bde27e6d
Sergey Shushlyapin <sshushlyapin@gmail.com>
2016-08-19 21:57:14 +0300

    Introduce internal FreezeOnParameterMatchCustomization class
    which is intended to extend the exising FreezeOnParameterMatch
    customization using the ParameterInfo property. That allows to create
    specimens using DataAnnotation attributes assigned to parameters.

---
+++ Src/AutoFixture.xUnit.net2/FreezeOnParameterMatchCustomization.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixture.Xunit2
+{
+    internal class FreezeOnParameterMatchCustomization : FreezeOnMatchCustomization
+    {
+        public FreezeOnParameterMatchCustomization(
+            ParameterInfo parameter,
+            IRequestSpecification matcher)
+            : base(parameter.ParameterType, matcher)
+        {
+            this.Request = parameter;
+        }
+    }
+}
